262062d2535376d8a869593a95af8b17ac429c1d
KangWon LEE <kangwon@gmail.com>
2024-03-31 19:43:00 +0900

    implement tests for one step of bisection method

--- tests/test_wk05.py
+++ tests/test_wk05.py
@@ -1,15 +1,24 @@
+'''
+Unit tests for one step of the bisction method
+The function under test is wk05.wk05()
+
+Would test the following:
+1. the binary point is smaller than root
+2. the binary point is larger than root
+3. both lower end upper bounds are smaller than root
+4. both lower end upper bounds are larger than root
+'''
+
 import math
 import pathlib
 import random
 import sys
 
-from typing import Callable, Generator, Dict, Tuple
+from typing import Callable
 
 
 import pytest
 
-
-RESULT = Dict[str, (float | bool)]
 
 file_path = pathlib.Path(__file__)
 test_folder = file_path.parent.absolute()
@@ -28,132 +37,158 @@ import wk05
 random.seed()
 
 
-def one_poly_root() -> float:
-    return ((0.5 + (random.random()*0.5)) * random.choice((-1, 1)))
-
-
-def poly_roots() -> Tuple[float]:
-    result = [one_poly_root(), one_poly_root()]
-
-    result.sort()
-
-    return tuple(result)
-
-
-def exp_coef() -> float:
-    return (random.random() * 8.0) + 2.0
-
-
-def get_poly_2(poly_roots:Tuple[float]) -> Callable[[float], float]:
-    def poly_2(x:float) -> float:
-        return ((x - poly_roots[0]) * (x - poly_roots[1]))
-    return poly_2
-
-
-def get_exp(exp_a:float, exp_b:float) -> Callable[[float], float]:
-    def exp(x:float) -> float:
-        return exp_a * math.exp(x) + (exp_b * (-1.0))
-    return exp
-
-
-def get_delta_x() -> float:
-    return ((random.random() * 0.5) + 0.5)
-
-
-def get_epsilon() -> float:
-    exp = random.randint(6, 8)
-    sig = random.randint(1, 9)
-
-    return ((0.1 ** exp) * sig)
-
-
-def generate_test_cases() -> Generator[RESULT, None, None]:
-
-    for found in (True, False):
-        yield (generate_poly_case(found))
-        yield (generate_exp_case(found))
-
-
-def generate_poly_case(found:bool):
-    p_roots = poly_roots()
-
-    delta_x = get_delta_x()
-
-    d_poly = {
-            'found': found,
-            'x': p_roots[0],
-            'delta_x': delta_x,
-            'f': get_poly_2(p_roots),
-            'epsilon': get_epsilon(),
-        }
-
-    if not found:
-        d_poly['x'] += ((-10.0) * delta_x)
-
-    d_poly['xp'] = (d_poly['x'] - d_poly['delta_x'])
-    return d_poly
-
-
-def generate_exp_case(found:bool):
-    exp_a = exp_coef()
-    exp_b = exp_a * exp_coef()
-
-    exp_root = math.log(exp_b / exp_a)
-
-    delta_x = get_delta_x()
-
-    d_exp = {
-            'found': found,
-            'x': exp_root,
-            'delta_x': delta_x,
-            'f': get_exp(exp_a, exp_b),
-            'epsilon': get_epsilon(),
-        }
-
-    if not found:
-        d_exp['x'] += ((-10.0) * delta_x)
-
-    d_exp['xp'] = (d_exp['x'] - d_exp['delta_x'])
-    return d_exp
+@pytest.fixture
+def root() -> float:
+    return random.uniform(-10, 10)
 
 
 @pytest.fixture
-def result_expected(request) -> Tuple[RESULT]:
-    d = request.param
-    d_result = wk05.wk05(
-        d['f'],
-        d['xp'],
-        d['delta_x'],
-        d['epsilon'],
-    )
-    d_expected = {
-        'x':d['x'],
-        'found':d['found'],
-    }
+def f(root:float) -> Callable[[float], float]:
+    '''
+    Returns an exponential function with a root at the given.
+    '''
+    a = random.uniform(2.0, 10.0)
+    b = -a * math.exp(root)
 
-    return (d_result, d_expected)
+    def return_this(x:float) -> float:
+        return (a * math.exp(x)) + b
+
+    assert math.isclose(return_this(root), 0.0)
+
+    return return_this
 
 
-@pytest.mark.parametrize("result_expected", generate_test_cases(), indirect=True)
-def test_check_values(result_expected:Tuple[RESULT]):
-    result, expected = result_expected
+@pytest.fixture
+def small_epsilon() -> float:
+    return 1e-6 * random.uniform(1.0, 9.0)
 
-    assert (result is not None), f"return value is None"
 
-    assert (len(result) == len(expected)), (
-        f"return value size={len(result)}, expected size = {len(expected)}"
-    )
+@pytest.fixture
+def big_epsilon() -> float:
+    return 1e-1 * random.uniform(1.0, 9.0)
 
-    assert (result.keys() == expected.keys()), (
-        f"return value keys={result.keys()}, expected keys = {expected.keys()}"
-    )
 
-    assert math.isclose(
-        result['x'], expected['x']
-    ), f"expected x = {expected['x']}, result x = {result['x']}"
-    assert (result['found'] == expected['found']), (
-        f"expected found = {expected['found']}, result found = {result['found']}"
-    )
+def test__choose_upper__not_found(root:float, f:Callable[[float], float], small_epsilon:float):
+
+    x_next = root + random.uniform(10.0, 5.0)
+    assert x_next > root
+
+    delta = random.uniform(20.0, 30.0)
+
+    x_lower = x_next + (-delta)
+    x_upper = x_next + delta
+
+    assert math.isclose(abs(x_lower - x_next), abs(x_upper - x_next))
+    assert (f(x_lower) * f(x_upper)) < 0
+
+    d = wk05.wk05(f, x_lower, x_upper, epsilon=small_epsilon)
+
+    assert not d['found']
+    assert math.isclose(d['x_upper'], x_next)
+    assert math.isclose(d['x_lower'], x_lower)
+
+
+def test__choose_lower__not_found(root:float, f:Callable[[float], float], small_epsilon:float):
+
+    x_next = root + random.uniform(-10.0, -5.0)
+    assert x_next < root
+
+    delta = random.uniform(20.0, 30.0)
+
+    x_lower = x_next + (-delta)
+    x_upper = x_next + delta
+
+    assert math.isclose(abs(x_lower - x_next), abs(x_upper - x_next))
+    assert (f(x_lower) * f(x_upper)) < 0
+
+    d = wk05.wk05(f, x_lower, x_upper, epsilon=small_epsilon)
+
+    assert not d['found']
+    assert math.isclose(d['x_upper'], x_upper)
+    assert math.isclose(d['x_lower'], x_next)
+
+
+def test__choose_upper__found(root:float, f:Callable[[float], float], big_epsilon:float):
+
+    x_next = root + random.uniform(0.05*big_epsilon, 0.1*big_epsilon)
+    assert root < x_next
+
+    delta = random.uniform(big_epsilon*0.1, big_epsilon*0.5)
+
+    x_lower = x_next + (-delta)
+    x_upper = x_next + delta
+
+    assert x_next < x_upper
+    assert x_lower < x_next
+
+    assert root < x_upper
+    assert x_lower < root
+
+    assert math.isclose(abs(x_lower - x_next), abs(x_upper - x_next))
+    assert (f(x_lower) * f(x_upper)) < 0
+
+    d = wk05.wk05(f, x_lower, x_upper, epsilon=big_epsilon)
+
+    assert d['found']
+    assert math.isclose(d['x_upper'], x_next)
+    assert math.isclose(d['x_lower'], x_lower)
+
+
+def test__choose_lower__found(root:float, f:Callable[[float], float], big_epsilon:float):
+
+    x_next = root + random.uniform((-0.1)*big_epsilon, (-0.05)*big_epsilon)
+    assert x_next < root
+
+    delta = random.uniform(big_epsilon*(0.1), big_epsilon*(0.5))
+
+    x_lower = x_next + (-delta)
+    x_upper = x_next + delta
+
+    assert x_next < x_upper
+    assert x_lower < x_next
+
+    assert root < x_upper
+    assert x_lower < root
+
+    assert math.isclose(abs(x_lower - x_next), abs(x_upper - x_next))
+    assert (f(x_lower) * f(x_upper)) < 0
+
+    d = wk05.wk05(f, x_lower, x_upper, epsilon=big_epsilon)
+
+    assert d['found']
+    assert math.isclose(d['x_upper'], x_upper)
+    assert math.isclose(d['x_lower'], x_next)
+
+def test__both_below(root:float, f:Callable[[float], float], small_epsilon:float):
+    x_lower = root + random.uniform(-10.0, -6.0)
+    x_upper = root + random.uniform(-5.0, -1.0)
+
+    assert x_lower < x_upper
+
+    assert (f(x_lower) * f(x_upper)) > 0
+
+    try:
+        d = wk05.wk05(f, x_lower, x_upper, epsilon=small_epsilon)
+    except ValueError:
+        pass
+    else:
+        assert False, "Should have raised a ValueError"
+
+
+def test__both_above(root:float, f:Callable[[float], float], small_epsilon:float):
+    x_lower = root + random.uniform(1.0, 5.0)
+    x_upper = root + random.uniform(6.0, 10.0)
+
+    assert x_lower < x_upper
+
+    assert (f(x_lower) * f(x_upper)) > 0
+
+    try:
+        d = wk05.wk05(f, x_lower, x_upper, epsilon=small_epsilon)
+    except ValueError:
+        pass
+    else:
+        assert False, "Should have raised a ValueError"
 
 
 if "__main__" == __name__:
