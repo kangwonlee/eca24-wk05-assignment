f9dc44add5dd09a7b934468af809af72a970f73b
KangWon LEE <kangwon@gmail.com>
2024-04-30 11:38:58 +0900

    add project path to sys.path

--- tests/test_grammar.py
+++ tests/test_grammar.py
@@ -1,6 +1,7 @@
 import ast
 import logging
 import pathlib
+import sys
 
 import pytest
 
@@ -8,6 +9,10 @@ import pytest
 file_path = pathlib.Path(__file__)
 test_folder = file_path.parent.absolute()
 proj_folder = test_folder.parent.absolute()
+
+
+sys.path.insert(0, str(proj_folder))
+
 
 logger = logging.getLogger(__file__)
 logger.setLevel(logging.INFO)
@@ -42,3 +47,4 @@ def test_module(py_file:pathlib.Path):
 
 def test_importable():
     import main
+    assert main is not None
