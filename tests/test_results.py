a6ebe188993c4e447276d5459b123dda6ef0baeb
KangWon LEE <kangwon@gmail.com>
2024-05-16 18:13:31 +0900

    update pytest messages

--- tests/test_results.py
+++ tests/test_results.py
@@ -83,12 +83,18 @@ def test__choose_upper__not_found(root:float, f:Callable[[float], float], small_
 
     d = main.wk05(f, x_lower, x_upper, epsilon=small_epsilon)
 
-    assert not d['found']
+    assert not d['found'], "Please check whether the root found.\n근을 찾은 것이 맞는지 확인 바람."
 
     assert f(d['x_upper']) * f(d['x_lower']) < 0, "The function values of both two bounds have the same signs. 상한과 하한에서 함수값의 부호가 다르지 않음."
 
-    assert math.isclose(d['x_upper'], x_next)
-    assert math.isclose(d['x_lower'], x_lower)
+    assert math.isclose(d['x_upper'], x_next), (
+        f"Expected `x_upper` to be close to {x_next}, but got {d['x_upper']}\n"
+        f"`x_upper` 값이 {x_next} 와 가까울 것으로 예상되었으나, {d['x_upper']} 을(를) 반환함\n"
+    )
+    assert math.isclose(d['x_lower'], x_lower), (
+        f"Expected `x_lower` to be close to {x_lower}, but got {d['x_lower']}\n"
+        f"`x_upper` 값이 {x_lower} 와 가까울 것으로 예상되었으나, {d['x_lower']} 을(를) 반환함\n"
+    )
 
 
 def test__choose_lower__not_found(root:float, f:Callable[[float], float], small_epsilon:float):
@@ -106,12 +112,18 @@ def test__choose_lower__not_found(root:float, f:Callable[[float], float], small_
 
     d = main.wk05(f, x_lower, x_upper, epsilon=small_epsilon)
 
-    assert not d['found']
+    assert not d['found'], "Please check whether the root found.\n근을 찾은 것이 맞는지 확인 바람."
 
     assert f(d['x_upper']) * f(d['x_lower']) < 0, "The function values of both two bounds have the same signs. 상한과 하한에서 함수값의 부호가 다르지 않음."
 
-    assert math.isclose(d['x_upper'], x_upper)
-    assert math.isclose(d['x_lower'], x_next)
+    assert math.isclose(d['x_upper'], x_upper), (
+        f"Expected `x_upper` to be close to {x_upper}, but got {d['x_upper']}\n"
+        f"`x_upper` 값이 {x_upper} 와 가까울 것으로 예상되었으나, {d['x_upper']} 을(를) 반환함\n"
+    )
+    assert math.isclose(d['x_lower'], x_next), (
+        f"Expected `x_lower` to be close to {x_next}, but got {d['x_lower']}\n"
+        f"`x_upper` 값이 {x_next} 와 가까울 것으로 예상되었으나, {d['x_lower']} 을(를) 반환함\n"
+    )
 
 
 def test__choose_upper__found(root:float, f:Callable[[float], float], big_epsilon:float):
@@ -135,12 +147,18 @@ def test__choose_upper__found(root:float, f:Callable[[float], float], big_epsilo
 
     d = main.wk05(f, x_lower, x_upper, epsilon=big_epsilon)
 
-    assert d['found']
+    assert d['found'], "Please check whether the root found.\n근을 찾은 것이 맞는지 확인 바람."
 
     assert f(d['x_upper']) * f(d['x_lower']) < 0, "The function values of both two bounds have the same signs. 상한과 하한에서 함수값의 부호가 다르지 않음."
 
-    assert math.isclose(d['x_upper'], x_next)
-    assert math.isclose(d['x_lower'], x_lower)
+    assert math.isclose(d['x_upper'], x_next), (
+        f"Expected `x_upper` to be close to {x_next}, but got {d['x_upper']}\n"
+        f"`x_upper` 값이 {x_next} 와 가까울 것으로 예상되었으나, {d['x_upper']} 을(를) 반환함\n"
+    )
+    assert math.isclose(d['x_lower'], x_lower), (
+        f"Expected `x_lower` to be close to {x_lower}, but got {d['x_lower']}\n"
+        f"`x_upper` 값이 {x_lower} 와 가까울 것으로 예상되었으나, {d['x_lower']} 을(를) 반환함\n"
+    )
 
 
 def test__choose_lower__found(root:float, f:Callable[[float], float], big_epsilon:float):
@@ -164,12 +182,19 @@ def test__choose_lower__found(root:float, f:Callable[[float], float], big_epsilo
 
     d = main.wk05(f, x_lower, x_upper, epsilon=big_epsilon)
 
-    assert d['found']
+    assert d['found'], "Please check whether the root found.\n근을 찾은 것이 맞는지 확인 바람."
 
     assert f(d['x_upper']) * f(d['x_lower']) < 0, "The function values of both two bounds have the same signs. 상한과 하한에서 함수값의 부호가 다르지 않음."
 
-    assert math.isclose(d['x_upper'], x_upper), ""
-    assert math.isclose(d['x_lower'], x_next)
+    assert math.isclose(d['x_upper'], x_upper), (
+        f"Expected `x_upper` to be close to {x_upper}, but got {d['x_upper']}\n"
+        f"`x_upper` 값이 {x_upper} 와 가까울 것으로 예상되었으나, {d['x_upper']} 을(를) 반환함\n"
+    )
+    assert math.isclose(d['x_lower'], x_next), (
+        f"Expected `x_lower` to be close to {x_next}, but got {d['x_lower']}\n"
+        f"`x_upper` 값이 {x_next} 와 가까울 것으로 예상되었으나, {d['x_lower']} 을(를) 반환함\n"
+    )
+
 
 def test__both_below(root:float, f:Callable[[float], float], small_epsilon:float):
     x_lower = root + random.uniform(-10.0, -6.0)
